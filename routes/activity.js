9d5ae54a7df8bc7e6ef14300a5ff82babcfd5c81
Mishra <pranmishra@deloitte.com>
2025-04-02 13:41:57 +0530

    custom activity changed

--- routes/activity.js
+++ routes/activity.js
@@ -63,7 +63,7 @@ exports.edit = function (req, res) {
     
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+   // logData(req);
     res.send(200, 'Edit');
 };
 
@@ -81,7 +81,7 @@ exports.save = function (req, res) {
     
     // Data from the req and put it in an array accessible to the main app.
     console.log( req.body );
-    logData(req);
+    //logData(req);
     res.send(200, 'Save');
 };
 
@@ -368,7 +368,7 @@ exports.publish = function (req, res) {
     
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     res.send(200, 'Publish');
 };
 
@@ -400,7 +400,7 @@ exports.stop = function (req, res) {
     
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    logData(req);
+    //logData(req);
     res.send(200, 'Validate');
 };
 
